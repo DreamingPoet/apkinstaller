807897b992318ef3703c00b38d5b28e0622a2f82
linjiangyuan <linjiangyuan>
2025-03-11 16:30:43 +0800

    增加授权安装apk功能

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,50 @@ import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 import { getCurrentWebview } from "@tauri-apps/api/webview";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = 
+let appNameEl: HTMLElement | null;
+let installResultEl: HTMLElement | null;
+let installFilePath: string = "";
 
-    
-    await invoke("greet", {
-      name: greetInputEl.value,
-    });
+// async function greet() {
+//   if (greetMsgEl && greetInputEl) {
+//     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//     greetMsgEl.textContent =
+//       await invoke("greet", {
+//         name: greetInputEl.value,
+//       });
+//   }
+// }
 
 
+
+async function dropFile(filePath: string) {
+
+  if (appNameEl) {
+    appNameEl.textContent = await invoke("drop_file", {
+      path: filePath
+    })
+    installFilePath = filePath;
   }
 }
 
+
+async function installAPK() {
+  if (installResultEl) {
+    installResultEl.textContent = await invoke("install", {
+      path: installFilePath
+    })
+
+    console.log('installResult')
+  }
+}
+
+
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  appNameEl = document.querySelector("#app-name");
+  installResultEl = document.querySelector("#install-result");
+  document.querySelector("#install-form")?.addEventListener("click", () => {
+    installAPK();
   });
 });
 
@@ -34,7 +55,8 @@ await getCurrentWebview().onDragDropEvent((event) => {
     // console.log('User hovering', event.payload.position);
   } else if (event.payload.type === 'drop') {
     console.log('User dropped', event.payload.paths);
+    dropFile(event.payload.paths[0])
   } else {
     console.log('File drop cancelled');
   }
- });
+});
